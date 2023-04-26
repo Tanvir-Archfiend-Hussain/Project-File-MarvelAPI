62c09f448de1891a499e17a3fa7d0c3c0fc17266
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-04-26 18:58:14 +0100

    CSS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/** 
+fetch("https://api.waifu.im/ass")
+  .then(res => res.json())
+  .then(data => console.log(data))
+*/
+/**  
+
+  fetch('https://reqres.in/api/users')
+  .then(res => res.json())
+  .then(data => console.log(data))
+  
+  */
+
+fetch("https://api.waifu.im/ass")
+  .then(x)
+  .then((data) => console.log(data));
