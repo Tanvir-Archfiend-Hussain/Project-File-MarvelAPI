ec485b1e2cdd5be8ccc1c29cbb33568f25b363b8
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-04-28 14:55:19 +0100

    SCRIPT

--- TryO-01/script.js
+++ TryO-01/script.js
@@ -1,7 +1,19 @@
-fetch('https://cdn.waifu.im/7682.jpg').then(res => {
+fetch('https://cdn.waifu.im/6618.jpeg').then(res => {
     console.log(res)
     return res.blob()
 }).then(res => {
     console.log(res)
     document.getElementById('rainbow').src = URL.createObjectURL(res);
+})
+
+fetch('https://cdn.waifu.im/4828.jpeg').then(roseB => {
+    console.log(roseB)
+    return roseB.blob()
+}).then(roseB => {
+    console.log(roseB)
+    document.getElementById('rose_bud').src = URL.createObjectURL(roseB);
+})
+
+fetch('https://cdn.waifu.im/4329.jpeg').then(ecchi => {
+    return ecchi.blob()
 })
