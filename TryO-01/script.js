55695d7a1d0d41dc8445508dbd66d2e356075d30
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-04-28 14:47:07 +0100

    JavaScript

--- TryO-01/script.js
+++ TryO-01/script.js
@@ -1,3 +1,7 @@
-fetch('rainbow.jpg').then(res => {
-    console.log()
+fetch('https://cdn.waifu.im/7682.jpg').then(res => {
+    console.log(res)
+    return res.blob()
+}).then(res => {
+    console.log(res)
+    document.getElementById('rainbow').src = URL.createObjectURL(res);
 })
