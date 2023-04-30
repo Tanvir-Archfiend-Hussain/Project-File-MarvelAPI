490a88cf06e5160dd23a650d1de5250af993ce2b
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-04-30 15:03:12 +0100

    JAVASCRIPT

--- proTryOUT/CountryAPi/JS/script.js
+++ proTryOUT/CountryAPi/JS/script.js
@@ -1,9 +1,13 @@
-let searchBtn = document.getElementById("search-btn")
-
-let countryInp = document.getElementById("country-inp")
-
-searchBtn.addEventListener("click", () => {
-    let countryName = countryInp.value;
-    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalURL)
+fetch("https://papi-pornstarsapi.p.rapidapi.com/pornstars/?ordering=-nationality", {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "<your rapid api key>",
+    "x-rapidapi-host": "papi-pornstarsapi.p.rapidapi.com"
+  }
 })
+.then(response => {
+  console.log(response);
+})
+.catch(err => {
+  console.error(err);
+});
